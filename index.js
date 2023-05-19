7a6225400d9dacf63e0f4cf04b30b4806925492e
r.idiyatov <r.idiyatov@iilab.ru>
2023-05-19 18:51:51 +0300

    feat: link to the front

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
