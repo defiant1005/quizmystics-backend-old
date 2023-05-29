9afacaf5dcc5fca42a520daeb031f346d4b37226
r.idiyatov <r.idiyatov@iilab.ru>
2023-05-29 23:33:24 +0300

    wip

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 
+const m = (name, text, id) => ({name, text, id})
+
 const PORT = 3000;
 
 const io = new Server(server, {
@@ -14,34 +16,15 @@ const io = new Server(server, {
     }
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-
-    console.log('a user connected');
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-
-    io.on("connection", (socket) => {
-        socket.emit("hello", "world");
-    });
-
     socket.on('userJoined', ((data, cb) => {
         if (!data.name || !data.room) {
             return cb('Данные некорректны')
         }
-
         cb({userId: socket.id})
+
+        socket.emit('newMessage', m('admin', `Добро пожаловать ${data.name}`))
     }))
-
-
 });
 
 
