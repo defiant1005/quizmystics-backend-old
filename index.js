85d4773ae021c54a0119687837a2900b6df716f5
r.idiyatov <r.idiyatov@iilab.ru>
2023-05-29 20:51:43 +0300

    wip

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", 'http://192.168.0.101:5173'],
+        origin: ["http://localhost:5173", 'http://127.0.0.1:5173', 'http://192.168.0.101:5173'],
         methods: ["GET", "POST"],
         credentials: true
     }
