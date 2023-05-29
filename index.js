810ee18943aa57d22e720ef59ab48e534792afa9
r.idiyatov <r.idiyatov@iilab.ru>
2023-05-29 22:16:18 +0300

    wip

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+
+const PORT = 3000;
+
 const io = new Server(server, {
     cors: {
         origin: ["http://localhost:5173", 'http://127.0.0.1:5173', 'http://192.168.0.101:5173'],
@@ -25,8 +28,24 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
+
+    io.on("connection", (socket) => {
+        socket.emit("hello", "world");
+    });
+
+    socket.on('userJoined', ((data, cb) => {
+        if (!data.name || !data.room) {
+            return cb('Данные некорректны')
+        }
+
+        cb({userId: socket.id})
+    }))
+
+
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+
+
+server.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
 });
