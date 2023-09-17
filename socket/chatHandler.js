745b3dca4639aba5cdebe3e61a5efe4684e37503
r.idiyatov <r.idiyatov@iilab.ru>
2023-09-17 21:47:04 +0300

    fix(global): bug fix

--- socket/chatHandler.js
+++ socket/chatHandler.js
@@ -3,17 +3,12 @@ const { Question } = require("../models/models");
 const { randomIntInclusive } = require("../helpers/get-random-int-inclusive");
 
 module.exports = (io) => {
-  // let allQuestion = [];
-  // let allPlayers = [];
-  // let usersCount = 0;
-  // let isGameStarted = false;
-  // let gameRoom = null;
-  // let questionNumber = 0;
   const rooms = {};
 
   const createRoom = function ({ name, room }, cb) {
     rooms[room] = {
-      allQuestion: [],
+      currentQuestions: [],
+      allQuestions: [],
       allPlayers: [],
       usersCount: 0,
       isGameStarted: false,
@@ -115,11 +110,11 @@ module.exports = (io) => {
     rooms[room].questionNumber += 1;
     const question_index = await randomIntInclusive(
       0,
-      rooms[room].allQuestion.length - 1,
+      rooms[room].currentQuestions.length - 1,
     );
     try {
-      const questionId = rooms[room].allQuestion[question_index].id;
-      rooms[room].allQuestion = rooms[room].allQuestion.filter(
+      const questionId = rooms[room].currentQuestions[question_index].id;
+      rooms[room].currentQuestions = rooms[room].currentQuestions.filter(
         (item) => item.id !== questionId,
       );
 
@@ -127,20 +122,21 @@ module.exports = (io) => {
     } catch (e) {
       console.error("ошибка в nextQuestion", {
         question_index,
-        allQuestionLength: rooms[room].allQuestion.length,
-        question: rooms[room].allQuestion[question_index],
+        currentQuestionsLength: rooms[room].currentQuestions.length,
+        question: rooms[room].currentQuestions[question_index],
       });
     }
   };
 
   const startGame = async function ({ room, players }, cb) {
-    rooms[room].usersCount = 0;
-    rooms[room].questionNumber = 0;
     const questions = await Question.findAll();
 
+    rooms[room].usersCount = 0;
+    rooms[room].questionNumber = 0;
     rooms[room].isGameStarted = true;
     rooms[room].allPlayers = players;
-    rooms[room].allQuestion = questions;
+    rooms[room].currentQuestions = questions;
+    rooms[room].allQuestions = questions;
     rooms[room].gameRoom = room;
 
     const questionId = await nextQuestion(room);
@@ -154,28 +150,22 @@ module.exports = (io) => {
   const disconnect = function (orderId, callback) {};
 
   const changeUserCount = async function ({ id, answer, userId, room }, cb) {
-    if (!room) {
-      cb("Поле room обязательное");
+    if (!room || !rooms[room]) {
+      cb(`Комнаты ${room} не существует`);
       return;
     }
 
-    let questions = [];
-
-    if (rooms[room]?.questionNumber === 1) {
-      questions = await Question.findAll();
-    }
     rooms[room].usersCount += 1;
 
-    const currentQuestion = questions.find((question) => question.id === id);
+    const currentQuestion = rooms[room].allQuestions.find(
+      (question) => question.id === id,
+    );
 
     rooms[room].allPlayers.forEach((player) => {
       if (player.userId === userId) {
         player.oldCount = player.count;
 
         if (answer === "" || answer === undefined || answer === null) {
-          console.table({
-            answer: answer,
-          });
           player.count += -100;
         } else {
           try {
@@ -191,17 +181,11 @@ module.exports = (io) => {
       }
     });
 
-    console.table({
-      usersCount: rooms[room].usersCount,
-      allPlayers: rooms[room].allPlayers.length,
-    });
-
     if (
       rooms[room].usersCount === rooms[room].allPlayers.length &&
       rooms[room].questionNumber < 10
     ) {
       rooms[room].usersCount = 0;
-
       const questionId = await nextQuestion(room);
 
       io.to(rooms[room].gameRoom).emit("finishQuestion", {
@@ -210,11 +194,7 @@ module.exports = (io) => {
           nextQuestion: questionId,
         },
       });
-    } else {
-      console.table({
-        gameRoom: rooms[room].gameRoom,
-      });
-
+    } else if (rooms[room].questionNumber === 10) {
       io.to(rooms[room].gameRoom).emit("finishGame", {
         data: {
           users: rooms[room].allPlayers,
