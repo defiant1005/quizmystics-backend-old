07232a4e4e1ce63df01136d21988bee1efd7b440
r.idiyatov <r.idiyatov@iilab.ru>
2023-10-06 01:13:04 +0300

    feat(global): change port

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ const errorHandling = require("./middleware/ErrorHandlingMiddleware");
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:5173",
-      "http://127.0.0.1:5173",
-      "http://192.168.0.101:5173",
-      "http://192.168.0.100:5173",
+      "http://localhost:8080",
+      "http://127.0.0.1:8080",
+      "http://192.168.0.101:8080",
+      "http://192.168.0.100:8080",
     ],
     methods: ["GET", "POST"],
     credentials: true,
